97dec58f6e3698f0d19e8721ad485e3ba69e28a2
EC2 Default User <ec2-user@ip-172-31-45-180.ap-southeast-1.compute.internal>
2019-02-03 04:59:08 +0000

    ツイートにユーザー情報を追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
+  
+  before_action :move_to_index,except: :index
+  
   def index
     @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -7,11 +10,17 @@ class TweetsController < ApplicationController
   end
   
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
   
   private
   def tweet_params
     params.permit(:name,:image,:text)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
